1fad71a79f8c2a0e79f2bc42715190219d5612ad
Caroline Berg <carolineeberg@gmail.com>
2018-06-19 18:02:14 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,11 @@
 def display_board
-  puts "A Tic Tac Toe Board"
-    row_one = "   |   |   "
-    row_two = "-----------"
-    puts row_one
-    puts row_two
-    puts row_one
-    puts row_two
-    puts row_one
+  row_one = "   |   |   "
+  row_two = "-----------"
+  puts row_one
+  puts row_two
+  puts row_one
+  puts row_two
+  puts row_one
+  puts row_two
+  puts row_one
 end
